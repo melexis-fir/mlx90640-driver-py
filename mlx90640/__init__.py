5956aa2e4c95d0e02b8b7a63c55c22b8c3707b68
mlx-kva <kva@melexis.com>
2021-03-09 12:49:10 -0800

    correct workflow + update test for 90640

--- mlx90640/__init__.py
+++ mlx90640/__init__.py
@@ -3,7 +3,7 @@ from .mlx90640 import MLX90640
 __import__('pkg_resources').declare_namespace(__name__)
 
 
-__version__ = '1.1.1'
+__version__ = '1.1.2'
 
 
 # try to pre-load our 3 default I2C low level implementations!
